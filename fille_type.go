0f7a274eeec8dd08c8923298eaf14c6d1fd75d42
storezhang <storezhang@gmail.com>
2020-08-13 14:33:34 +0800

    增加文件类型

--- fille_type.go
+++ fille_type.go
@@ -2,17 +2,18 @@ package yunke
 
 const (
 	// 文件类型
-	// 之所以要以10为间隔，是怕中途会半圆相类似的类型
 	// 产品文件
 	FileDirTypeProductFile DirType = "product"
 	// 产品发布
-	FileDirTypeProductRelease DirType = "release"
+	FileDirTypeProductRelease DirType = "product-release"
 	// 公共云盘
 	FileDirTypePublicDisk DirType = "public"
 	// 私有云盘
 	FileDirTypePrivateDisk DirType = "private"
 	// 普通文件
 	FileDirTypeFileResource DirType = "resource"
+	// 版本发布文件
+	FileDirTypeOrgRelease DirType = "org-release"
 )
 
 // 文件类型
